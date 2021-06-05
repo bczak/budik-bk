152e1c9c4e8680c43f2df4fe0bb7c31025e62932
Jakhongir Tashpulatov <tashpulatov.jakhongir@gmail.com>
2021-06-05 12:09:35 +0200

    backup

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './styles/index.css'
 import './styles/fonts/material-icons.css'
-// You need a specific loader for CSS files
-import 'vue-datetime/dist/vue-datetime.css'
-import {Quasar, QTime} from 'quasar'
 
 
-createApp(App).use(router).use(Quasar, {components: {QTime}}).mount('#app')
+createApp(App).use(router).mount('#app')
