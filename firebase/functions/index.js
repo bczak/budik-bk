c47e084ffd79c7b6d57f73e531fc6b524b46ce65
Jakhongir <tashpulatov.jakhongir@gmail.com>
2021-06-28 20:48:56 +0200

    Improved function to fetch times of alarm

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,6 +1,6 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const { DateTime } = require("luxon");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const { DateTime } = require('luxon')
 
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_API_KEY,
@@ -11,64 +11,80 @@ const firebaseConfig = {
 	appId: process.env.FIREBASE_APP_ID,
 	measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
-admin.initializeApp(firebaseConfig);
+admin.initializeApp(firebaseConfig)
 
 function parseRepeat(repeat) {
-	if (repeat === 0) return "0000000";
+	if (repeat === 0) return '0000000'
 	return (repeat >>> 0)
 		.toString(2)
-		.padStart(7, "0")
-		.split("")
+		.padStart(7, '0')
+		.split('')
 		.reverse()
-		.join("");
+		.join('')
 }
 
-exports.processAlarm = functions.firestore.document("alarms/{alarmId}").onWrite(async (snap, context) => {
+exports.processAlarm = functions.firestore.document('alarms/{alarmId}').onWrite(async (snap, context) => {
 	if (snap.after.exists) {
-		let alarm = snap.after.data();
+		let alarm = snap.after.data()
 		if (!alarm.status) {
-			await removeNotification(snap.after.id);
-			return;
+			await removeNotification(snap.after.id)
+			return
 		}
-		let times = getNextTimestamp(snap.after.data());
+		let times = getNextTimestamp(snap.after.data())
 		await admin
 			.firestore()
-			.collection("next")
+			.collection('next')
 			.doc(context.params.alarmId)
-			.set({ type: "alarm", times: times });
+			.set({ type: 'alarm', times: [...times] })
+		console.log(times)
 	} else {
-		await removeNotification(context.params.alarmId);
+		await removeNotification(context.params.alarmId)
 	}
-});
+})
 
 async function removeNotification(alarmId) {
 	await admin
 		.firestore()
-		.collection("next")
+		.collection('next')
 		.doc(alarmId)
-		.delete();
+		.delete()
 }
 
 function getNextTimestamp(alarm) {
-	let nextTime = alarm.time;
-	let repeat = parseRepeat(alarm.repeat);
-	let now = DateTime.now().setZone("Europe/Prague");
-	let modifiedRepeat = getNextDayOfWeek(repeat, now);
-	let times = [];
+	let nextTime = alarm.time
+	let repeat = parseRepeat(alarm.repeat)
+	let now = DateTime.now().setZone('Europe/Prague')
+	let modifiedRepeat = getNextDayOfWeek(repeat, now)
+	let times = []
+	// if no repeat but enabled
+	if (alarm.repeat === 0) {
+		let next = DateTime.now().set({
+			hour: nextTime.split(':')[0],
+			minute: nextTime.split(':')[1],
+			second: 0,
+			millisecond: 0
+		})
+		if (now.toFormat('T') > nextTime) {
+			times.push(next)
+		} else {
+			times.push(next.plus({ day: 1 }))
+		}
+	}
+
 	for (let i = 0; i < 7; i++) {
-		let now = DateTime.now().setZone("Europe/Prague");
-		let day = Number(modifiedRepeat[i]);
-		if (day === 0) continue;
+		let now = DateTime.now().setZone('Europe/Prague')
+		let day = Number(modifiedRepeat[i])
+		if (day === 0) continue
 		if (i === 0) {
 			// if today before now then move to next week
-			if (nextTime < now.toFormat("T")) {
-				now = now.plus({ week: 1 });
+			if (nextTime < now.toFormat('T')) {
+				now = now.plus({ week: 1 })
 			}
 		}
-		now = now.set({ hour: nextTime.split(":")[0], minute: nextTime.split(":")[1], second: 0, millisecond: 0 });
-		times.push(now.plus({ day: i }).toISO());
+		now = now.set({ hour: nextTime.split(':')[0], minute: nextTime.split(':')[1], second: 0, millisecond: 0 })
+		times.push(now.plus({ day: i }))
 	}
-	return times;
+	return [...(new Set(times.map(e => e.toISO())))];
 }
 
 /**
@@ -78,5 +94,5 @@ function getNextTimestamp(alarm) {
  * @param {DateTime} now
  */
 function getNextDayOfWeek(repeat, now) {
-	return repeat.substr(now.weekday - 1, 7 - now.weekday + 1) + repeat.substr(0, now.weekday - 1);
+	return repeat.substr(now.weekday - 1, 7 - now.weekday + 1) + repeat.substr(0, now.weekday - 1)
 }
